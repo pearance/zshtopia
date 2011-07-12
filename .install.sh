b1e2e0f7649f0a6e747c3f104ef26957b032f32c
Fontaine Cook <fontaine.cook@pearance.com>
2011-07-12 01:41:45 +0000

    Keep it .install.sh! Do not remove at end of install

--- .install.sh
+++ .install.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
 # Backup any remnants of an existing bash environment.
+#------------------------------------------------------------------------------
 mkdir -p ~/.bashtopia.backup
-for i in ~/.profile ~/.bashrc ~/.bash_aliases ~/.bash
+cd ~
+for i in .profile .bashrc .bash_aliases .bash
   do [ -e $i ] && mv -f $i bashtopia.backup/$i.bashtopia.bak
 done
+# Just in case they are symlinks, remove them.
 rm -f ~/.profile
 rm -f ~/.bashrc
 rm -f ~/.bash_aliases
+rm -rf ~/.bash
 echo -e "\nBackedup existing Bash environment successfully!\n"
 
 
 
 # Clone Bashtopia.
-rm -r ~/.bash
-git clone git://github.com/Bashtopia/Bashtopia.git ~/.bash
+#------------------------------------------------------------------------------
+git clone git@github.com:Bashtopia/Bashtopia.git ~/.bash
 echo -e "\nCloned Bashtopia successfully!\n"
 
 
 
 # Link to configuration files.
+#------------------------------------------------------------------------------
 ln -s ~/.bash/profile ~/.profile
 ln -s ~/.bash/bashrc ~/.bashrc
 ln -s ~/.bash/bash_aliases ~/.bash_aliases
@@ -28,7 +33,7 @@ echo -e "\nLinked to configuration files successfully!\n"
 
 
 # Clean up.
-rm ~/install.sh
-rm ~/.bash/install.sh
+#------------------------------------------------------------------------------
+rm ~/.install.sh
 echo -e "\nCleaned up successfully!\n"
 
