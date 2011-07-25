807ca17fea2bbceb686e65aa133a1d41473204ce
Fontaine Cook <fontaine.cook@pearance.com>
2011-07-25 09:03:23 +0000

    Add git configuration setup

--- .install.sh
+++ .install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# User input.
+clear
+echo -e "\nFor your git configuration, please enter your..."
+echo -e "\nFull Name:"
+read $USER_FULLNAME
+
+echo -e "\nEmail Address:"
+read $USER_EMAIL
+
+
+
 # Create Folders
 #------------------------------------------------------------------------------
 mkdir -p ~/trash/
@@ -37,8 +48,49 @@ echo -e "\nLinked to configuration files successfully!\n"
 
 
 # Generate git config file.
+generate_gitconfig >> ~/.gitconfig
+function  generate_gitconfig {
+cat << _EOF_
+[alias]
+  lg = log --graph --pretty=format:'%C(magenta)%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative
 
+[core]
+  editor = "vim -c start"
+  excludesfile = ~/.gitignore
 
+[diff]
+  tool = vimdiff
+
+[pager]
+  diff =
+
+[difftool]
+  prompt = false
+
+[color]
+  branch = auto
+  diff = auto
+  status = auto
+
+[color "branch"]
+  current = yellow reverse
+  local = magenta
+  remote = green
+
+[color "diff"]
+  meta = yellow bold
+  frag = magenta bold
+  old = red bold
+  new = white bold
+
+[color "status"]
+  added = green
+  changed = yellow
+  untracked = red
+_EOF_
+}
+git config --global user.name "$USER_FULLNAME"
+git config --global user.email "$USER_EMAIL"
 
 
 # Clean up.
