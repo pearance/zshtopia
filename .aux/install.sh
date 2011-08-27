99c53bc4be8176e72c055e2ae0de6ff820249bf0
Fontaine Cook <fontaine.cook@pearance.com>
2011-08-27 04:25:15 -0400

    Updated and renamed .install.sh to .aux/install.sh

--- .aux/install.sh
+++ .aux/install.sh
@@ -8,42 +8,46 @@ read $USER_FULLNAME
 
 echo -e "\nEmail Address:"
 read $USER_EMAIL
+echo -e "\nBackedup existing Bash environment successfully!\n"
+#------------------------------------------------------------------------------
 
 
 
 # Create Folders
-#------------------------------------------------------------------------------
 mkdir -p ~/trash/
 mkdir -p ~/tmp/
 mkdir -p ~/backup/
-
-
-
-# Backup any remnants of an existing bash environment.
+echo -e "\nBackedup existing Bash environment successfully!\n"
 #------------------------------------------------------------------------------
+
+
+
+# Backup any existing bash environment.
 cd ~
 mkdir -p ~/backup/bashtopia/
-for i in .profile .bashrc .bash_aliases .bash
+for i in .profile .bashrc .bash_aliases .bash .tmux.conf
   do [ -e $i ] && mv -f --backup=t $i backup/bashtopia/$i.bashtopia.bak
 done
 echo -e "\nBackedup existing Bash environment successfully!\n"
+#------------------------------------------------------------------------------
 
 
 
 # Clone Bashtopia.
-#------------------------------------------------------------------------------
-git clone git@github.com:Bashtopia/Bashtopia.git ~/.bash
+git clone git://github.com/Bashtopia/Bashtopia.git ~/.bash
 echo -e "\nCloned Bashtopia successfully!\n"
+#------------------------------------------------------------------------------
 
 
 
 # Link to configuration files.
-#------------------------------------------------------------------------------
 cd ~
 ln -s .bash/profile .profile
 ln -s .bash/bashrc .bashrc
 ln -s .bash/bash_aliases .bash_aliases
+ln -s .bash/tmux.conf .tmux.conf
 echo -e "\nLinked to configuration files successfully!\n"
+#------------------------------------------------------------------------------
 
 
 
@@ -91,10 +95,13 @@ _EOF_
 }
 git config --global user.name "$USER_FULLNAME"
 git config --global user.email "$USER_EMAIL"
+echo -e "\nGit configured successfully!\n"
+#------------------------------------------------------------------------------
+
 
 
 # Clean up.
-#------------------------------------------------------------------------------
 rm ~/.install.sh
 echo -e "\nCleaned up successfully!\n"
+#------------------------------------------------------------------------------
 
