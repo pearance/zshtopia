ae26639729b234b1ec0ca8d5426e1b940132d331
Fontaine Cook <fontaine cook@pearance.com>
2013-05-05 04:32:46 -0400

    Misc formating

--- .aux/install.sh
+++ .aux/install.sh
@@ -1,7 +1,23 @@
+#vim:fdm=marker:
 #!/bin/bash
+#          _     _              _
+#  _______| |__ | |_ ___  _ __ (_) __ _
+# |_  / __| '_ \| __/ _ \| '_ \| |/ _` |
+#  / /\__ \ | | | || (_) | |_) | | (_| |
+# /___|___/_| |_|\__\___/| .__/|_|\__,_|
+#                        |_|
+# (_)_ __  ___| |_ __ _| | |  ___| |__
+# | | '_ \/ __| __/ _` | | | / __| '_ \
+# | | | | \__ \ || (_| | | |_\__ \ | | |
+# |_|_| |_|___/\__\__,_|_|_(_)___/_| |_|
+
+# Author:         Fontaine Cook
+# Description:    Vimez installation script.
+# ------------------------------------------------------------------------------
+
 clear
 
-# Updates and installs.
+# UPDATES AND INSTALLS {{{
 aptitude update
 aptitude install git-core zsh tmux
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
@@ -9,8 +25,8 @@ echo -e "\nInstalled zsh successfully!\n"
 #------------------------------------------------------------------------------
 
 
-
-# Create Folders
+#}}}
+# CREATE FOLDERS {{{
 mkdir -p ~/bin/
 mkdir -p ~/backups/
 mkdir -p ~/trash/
@@ -18,8 +34,8 @@ echo -e "\nCreated folder structure successfully!\n"
 #------------------------------------------------------------------------------
 
 
-
-# Backup any existing environment.
+#}}}
+# BACKUP ANY EXISTING ENVIRONMENT {{{
 mkdir -p ~/backups/pre-zshtopia/
 
 for i in .zshrc .zsh .tmux.conf .zshrc.pre-oh-my-zsh .gitconfig
@@ -33,15 +49,15 @@ echo -e "\nBackedup existing environment successfully!\n"
 #------------------------------------------------------------------------------
 
 
-
-# Clone Zshtopia.
+#}}}
+# CLONE ZSHTOPIA {{{
 git clone https://github.com/zshtopia/zshtopia.git ~/.zsh
 echo -e "\nCloned zshtopia successfully!\n"
 #------------------------------------------------------------------------------
 
 
-
-# Link to configuration files.
+#}}}
+# LINK TO CONFIGURATION FILES {{{
 cd ~
 ln -s .zsh/zshrc ~/.zshrc
 ln -s .zsh/tmux.conf ~/.tmux.conf
@@ -50,8 +66,8 @@ echo -e "\nLinked configuration files successfully!\n"
 #------------------------------------------------------------------------------
 
 
-
-# User input.
+#}}}
+# USER iNPUT {{{
 echo -e "For your git configuration, please enter your..."
 read -p "Full Name: " fullname
 echo
@@ -59,17 +75,18 @@ read -p "Email Address: " email
 #------------------------------------------------------------------------------
 
 
-
-# Generate git config file.
+#}}}
+# GENERATE GIT CONFIG FILE {{{
 git config --global user.name "$fullname"
 git config --global user.email "$email"
 echo -e "\nGit configured successfully!\n"
 #------------------------------------------------------------------------------
 
 
-# Clean up.
+#}}}
+# CLEAN UP {{{
 #------------------------------------------------------------------------------
 rm ~/install.sh
 echo -e "\nCleaned up successfully!\n"
 zsh
-
+#}}}
